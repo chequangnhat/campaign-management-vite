03caf1580f93472db47d8fe7f74a10b169e1bfc7
nhat <nhatche42@gmail.com>
2022-11-25 11:38:46 +0700

    fix css

--- src/App.vue
+++ src/App.vue
@@ -11,11 +11,7 @@ import Login from './components/Login.vue'
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
