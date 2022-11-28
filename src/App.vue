4449012b388bed1f4080cc0e1976be8d6b7d07a2
nhat <nhatche42@gmail.com>
2022-11-28 11:54:25 +0700

    refactor

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,25 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-import Login from './components/Login.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import Login from "./views/Login.vue";
+import Campaign from "./views/Campaign.vue";
+import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
- 
-  <Login />
+  <!-- <Login /> -->
+  <!-- <Campaign /> -->
+  <nav>
+    <RouterLink to="/">Campaigns</RouterLink>
+    <RouterLink to="/test">test</RouterLink>
+  </nav>
+  <RouterView />
 </template>
 
 <style scoped>
-
 .logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+  filter: drop-shadow(0 0 2em #080808aa);
 }
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
---
+++ src/views/Campaign.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>hello guy, i am campaign page</div>
+</template>
+<script>
+export default {
+  // data: {
+  //   "campaign name": "name of campaign",
+  //   "status": "status name",
+  //   "used amount": "20",
+  //   "usage rate": "0.5",
+  //   "budget": "1000",
+  //   "start date": "2022-12-12 10:00",
+  //   "end date": "2022-12-1 10:00",
+  // },
+};
+</script>
+<style scoped>
+</style>
